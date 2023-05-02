3f3c2b15bd54509c5b7e088e0884b875e325dd35
Nikita <nmal4ik@gmail.com>
2023-05-02 19:42:06 +0100

    fix settings.py

--- puddle/settings.py
+++ puddle/settings.py
@@ -1,5 +1,5 @@
 import os
-import dj_database_url
+# import dj_database_url
 if os.path.isfile('env.py'):
     import env
 from pathlib import Path
@@ -12,14 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(5h&^god(7aicz#@q^sl_(w)2otp'
  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # DEBUG = False
 # heroku config:set DISABLE_COLLECTSTATIC=1
 
-ALLOWED_HOSTS = ['jacketshop.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login/'
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+# DATABASES = {
+#      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#  }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
