474f6b9cf6d9e0a9fbff481994a74142a53ff656
readytow0rk <>
2023-05-30 13:18:12 +0000

    .

---
+++ env.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ["SECRET_KEY"] = "django-insecure-j4ippt+3h39u4ontllpc8a(5h&^god(7aicz#@q^sl_(w)2otp"
+
+os.environ["DATABASE_URL"] = "postgres://dgrstcyf:R7aLnrp0ThZri8B4Uq1ngJL22qdqkihF@lucky.db.elephantsql.com/dgrstcyf"
+
+
+
+
+
+
+
+
